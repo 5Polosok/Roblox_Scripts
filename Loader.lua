e631e7ffecd282872c754547a63b793480cdab98
fd221 <eptar123@mail.ru>
2025-08-24 19:24:18 +0500

    Loader for all games in 1 script :)

---
+++ Loader.lua
@@ -0,0 +1,10 @@
+if game.PlaceId == 16946008847 then
+    --Anime Final Strike
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/5Polosok/Roblox_Scripts/refs/heads/main/Anime%20Final%20Strike/AnimeFinalStrikeloader.lua"))()
+elseif gane.PlaceId == 16146832113 then
+    --Anime Vanguards
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/5Polosok/Roblox_Scripts/refs/heads/main/Anime%20Vanguards/AVScriptLoader.lua"))()
+elseif game.PlaceId == 17687504411 then
+    --ASTDX
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/5Polosok/Roblox_Scripts/refs/heads/main/ASTDX/ASTDXLoader.lua"))()
+end

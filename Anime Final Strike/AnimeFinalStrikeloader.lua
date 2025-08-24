3eb4ed901ec3855b01a60ea7cb2c95c0f7013b33
fd221 <eptar123@mail.ru>
2025-08-24 19:28:58 +0500

    Changed AnimeFinalStrikeloader.lua logic

--- Anime Final Strike/AnimeFinalStrikeloader.lua
+++ Anime Final Strike/AnimeFinalStrikeloader.lua
@@ -1,17 +1,9 @@
 --AutoJoiner loadstring
 task.spawn(function()
-    --AutoJoiner Statements
-    getgenv().TpToEventStatement = false
-    getgenv().AmountOfAccounts = 4
     loadstring(game:HttpGet("https://raw.githubusercontent.com/5Polosok/Roblox_Scripts/refs/heads/main/Anime%20Final%20Strike/AutoJoiner.lua"))()
 end)
 
 --AutoClaimer loadstring
 task.spawn(function()
-    --AutoClaimer Statements
-    getgenv().BuyRRS = true
-    getgenv().CollectDaily = true
-    getgenv().SpinWheel = true
-    getgenv().RedeemCodes - true 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/5Polosok/Roblox_Scripts/refs/heads/main/Anime%20Final%20Strike/ClaimRewards.lua"))()
 end)

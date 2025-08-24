64a0b96314135f7e7bcc9a16d2e2feaa3c81f042
fd221 <eptar123@mail.ru>
2025-08-24 19:27:18 +0500

    Changed Logic of Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,25 @@
+--Anime Final Strike
 if game.PlaceId == 16946008847 then
-    --Anime Final Strike
+    --AutoJoiner Statements
+    getgenv().TpToEventStatement = false
+    getgenv().AmountOfAccounts = 4
+    --ClaimAll
+    getgenv().BuyRRS = true
+    getgenv().CollectDaily = true
+    getgenv().SpinWheel = true
+    getgenv().RedeemCodes - true 
+    --loadstring
     loadstring(game:HttpGet("https://raw.githubusercontent.com/5Polosok/Roblox_Scripts/refs/heads/main/Anime%20Final%20Strike/AnimeFinalStrikeloader.lua"))()
-elseif gane.PlaceId == 16146832113 then
+
     --Anime Vanguards
+elseif gane.PlaceId == 16146832113 then
+    --AutoClaimer statements
+    getgenv().redeemcodes = true
+    getgenv().claimlevel = true
     loadstring(game:HttpGet("https://raw.githubusercontent.com/5Polosok/Roblox_Scripts/refs/heads/main/Anime%20Vanguards/AVScriptLoader.lua"))()
+
+--ASTDX
 elseif game.PlaceId == 17687504411 then
-    --ASTDX
+    getgenv().UseParser = false
     loadstring(game:HttpGet("https://raw.githubusercontent.com/5Polosok/Roblox_Scripts/refs/heads/main/ASTDX/ASTDXLoader.lua"))()
 end

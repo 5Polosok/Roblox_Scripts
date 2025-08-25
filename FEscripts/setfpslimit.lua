e86d2694e3be2311ca9fcd802ea6d7e75b99bf75
fd221 <eptar123@mail.ru>
2025-08-25 16:49:15 +0500

    removed fpsamount value???

--- FEscripts/setfpslimit.lua
+++ FEscripts/setfpslimit.lua
@@ -1,8 +1,3 @@
-
 if getgenv().enable_fpscap then
-    if getgenv().fps_amount ~= nil and tonumber(getgenv().fps_amount) then 
-        setfpscap(getgenv().fps_amount)
-    else
-        setfpscap(20)
-    end
- end
+    setfpscap(20)
+end

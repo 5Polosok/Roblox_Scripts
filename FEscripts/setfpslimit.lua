5c6b936eb2126c2e5a1d347c5e5772042129623b
fd221 <eptar123@mail.ru>
2025-08-25 17:00:12 +0500

    Made checking for setfpscap function (holy delta)

--- FEscripts/setfpslimit.lua
+++ FEscripts/setfpslimit.lua
@@ -1,3 +1,14 @@
+getgenv().enable_fpscap = true
+getgenv().fps_amount = 20
+
 if getgenv().enable_fpscap then
-    setfpscap(20)
+    if type(setfpscap) == "function" then
+        if getgenv().fps_amount ~= nil and tonumber(getgenv().fps_amount) then 
+            setfpscap(getgenv().fps_amount)
+        else
+            setfpscap(20)
+        end
+    else
+        warn("setfpscap Function not found!")
+    end
 end

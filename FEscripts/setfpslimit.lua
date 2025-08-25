a6ae642c4c8472a2910952b1fbeaa9682a0da0e3
fd221 <eptar123@mail.ru>
2025-08-26 02:22:17 +0500

    added loop for setfpscap (holy arceus)

--- FEscripts/setfpslimit.lua
+++ FEscripts/setfpslimit.lua
@@ -2,9 +2,13 @@ if getgenv().enable_fpscap then
     if type(setfpscap) == "function" then
         warn("setfpscapisreal")
         if getgenv().fps_amount ~= nil and tonumber(getgenv().fps_amount) then 
-            setfpscap(getgenv().fps_amount)
+            while task.wait(10) do
+                setfpscap(getgenv().fps_amount)
+            end
         else
-            setfpscap(20)
+            while task.wait(10) do
+                setfpscap(20)
+            end
         end
     else
         warn("setfpscap Function not found!")

111e96dcc399c03c7d4ec9e59ecedd34be6ffc4b
fd221 <eptar123@mail.ru>
2025-08-25 17:07:22 +0500

    fixed setfpscap (again)

--- FEscripts/setfpslimit.lua
+++ FEscripts/setfpslimit.lua
@@ -1,8 +1,6 @@
-getgenv().enable_fpscap = true
-getgenv().fps_amount = 20
-
 if getgenv().enable_fpscap then
     if type(setfpscap) == "function" then
+        warn("setfpscapisreal")
         if getgenv().fps_amount ~= nil and tonumber(getgenv().fps_amount) then 
             setfpscap(getgenv().fps_amount)
         else

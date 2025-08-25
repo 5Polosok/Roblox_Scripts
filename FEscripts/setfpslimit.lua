64b331b8951eb26e14ebdd963d5f3e07f202f53c
fd221 <eptar123@mail.ru>
2025-08-26 03:33:03 +0500

    removed loops (holy lags)

--- FEscripts/setfpslimit.lua
+++ FEscripts/setfpslimit.lua
@@ -2,13 +2,9 @@ if getgenv().enable_fpscap then
     if type(setfpscap) == "function" then
         warn("setfpscapisreal")
         if getgenv().fps_amount ~= nil and tonumber(getgenv().fps_amount) then 
-            while task.wait(10) do
                 setfpscap(getgenv().fps_amount)
-            end
         else
-            while task.wait(10) do
                 setfpscap(20)
-            end
         end
     else
         warn("setfpscap Function not found!")
